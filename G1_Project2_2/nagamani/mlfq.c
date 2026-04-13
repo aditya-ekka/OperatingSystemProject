21a4d40d91eb404fb452a710af38b6d146e20f84
Nagamani53 <23je0949@iitism.ac.in>
2026-04-13 11:36:55 +0530

    Add MLFQ scheduling simulation in mlfq.c
    Implement a simple Multi-Level Feedback Queue (MLFQ) scheduling simulation in C.

---
+++ G1_Project2_2/nagamani/mlfq.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int bt[10], wt[10], tat[10];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("Enter burst time for process %d: ", i+1);
+        scanf("%d", &bt[i]);
+    }
+
+    // Simple MLFQ logic (2 queues simulation)
+    wt[0] = 0;
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i-1] + bt[i-1];
+    }
+
+    for(i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+    }
+
+    printf("\nProcess\tBT\tWT\tTAT\n");
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\n", i+1, bt[i], wt[i], tat[i]);
+    }
+
+    // Gantt Chart
+    printf("\nGantt Chart:\n| ");
+    for(i = 0; i < n; i++) {
+        printf("P%d | ", i+1);
+    }
+
+    return 0;
+}
